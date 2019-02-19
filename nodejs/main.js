2c0712a842529a26fa8b906aa1aab3930b8ab6c2
Lu Wenxi <wxilu@ucdavis.edu>
2019-02-19 02:05:22 -0800

    connect each client a specific engine

--- nodejs/main.js
+++ nodejs/main.js
@@ -2,30 +2,33 @@
 const vox = require('./module/index')
 
 let PullFromEngine = () => {
-  vox.engine.CallGetScene()
-  vox.engine.NotifyRequestFrame(null)
+  vox.engine1.CallGetScene()
+  vox.engine1.NotifyRequestFrame(null)
 }
 
-/* TODO what if there are multiple sockets connecting */
+for(let i = 0; i < 2; ++i) {
+  /* TODO what if there are multiple sockets connecting */
 const MainLoop = (socket) => {
+  
   //
   // setup engine
   //
-  vox.engine.OnFrame = (frame) => {
-    vox.client.Broadcast('hasNewFrame', frame)
+  
+  vox.engine[i].OnFrame = (frame) => {
+    vox.client[i].Broadcast('hasNewFrame', frame)
   }
-  vox.engine.OnGetScene = (scene) => {
+  vox.engine[i].OnGetScene = (scene) => {
     console.log('has new scene')
     socket.emit('hasNewScene', scene)
   }
-  vox.engine.OnQueryDatabase = (database) => {
+  vox.engine[i].OnQueryDatabase = (database) => {
     socket.emit('queryDatabase', database)
   }
   //
   // setup socket
   //
   socket.on('selectData', (data) => {
-    vox.engine.NotifyOpenProject(data.label)
+    vox.engine[i].NotifyOpenProject(data.label)
     PullFromEngine()
   })
   socket.on('disconnect', () => {
@@ -34,13 +37,13 @@ const MainLoop = (socket) => {
     // PullFromEngine()
   })
   socket.on('requestFrame', (params) => {
-    vox.engine.NotifyRequestFrame(params)
+    vox.engine[i].NotifyRequestFrame(params)
     // PullFromEngine()
   })
   //
   // Start the process
   //
-  vox.engine.CallQueryDatabase()
+  vox.engine[i].CallQueryDatabase()
   PullFromEngine()
 }
 
@@ -49,12 +52,14 @@ const MainLoop = (socket) => {
  */
 
 /* We have to run the server first */
-vox.client.OnConnect = MainLoop
-vox.engine.OnConnect = () => {
-  vox.client.serve(4000)
+vox.client[i].OnConnect = MainLoop
+vox.engine[i].OnConnect = () => {
+  vox.client[i].serve(4000)
 }
-vox.engine.OnProjectOpened = () => {
+vox.engine[i].OnProjectOpened = () => {
   console.log('opened')
   PullFromEngine()
 }
-vox.engine.connect(process.argv.length > 2 ? process.argv[2] : 'localhost')
+vox.engine[i].connect(process.argv.length > 2 ? process.argv[2] : 'localhost')
+}
+
