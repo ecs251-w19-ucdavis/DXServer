c265a6efa2d4196c6593de3bad15cf31f627de80
Lu Wenxi <wxilu@ucdavis.edu>
2019-03-04 19:48:02 -0800

    GPUTaskHandler

--- src/GPUTaskHandler.h
+++ src/GPUTaskHandler.h
@@ -5,11 +5,28 @@
 #ifndef DXSERVER_GPUTASKHANDLER_H
 #define DXSERVER_GPUTASKHANDLER_H
 
-
+/**
+ * This class suppose to handle requests from the event queue. This class will open and run in a different thread.
+ */
 class GPUTaskHandler
 {
+public:
+    /** This 
+     *       
+     */
+    explicit GPUTaskHandler();
+    void processNextRequest();
+
+private:
+    void initOpenGL();
+    void requestFrame();
+    void getScene();
+    void queryDatabase();
+
+private:
+    
 
 };
 
 
-#endif //DXSERVER_GPUTASKHANDLER_H
+#endif //DXSERVER_GPUTASKHANDLER_
